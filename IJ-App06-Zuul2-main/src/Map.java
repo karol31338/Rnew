aa225cc6717c0c6946d8517b6fb5ed1e9dd8454f
karol31338 <71914846+karol31338@users.noreply.github.com>
2020-12-01 19:23:06 +0000

    1

---
+++ IJ-App06-Zuul2-main/src/Map.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ *  This is a comment
+ */
+public class Map
+{
+    private ArrayList<Room> room;
+
+    public Map()
+{
+    room = new ArrayList<Room>();
+}
+}
